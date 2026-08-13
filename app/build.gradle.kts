3257dcae1ec3c9ae5c55849b0239f96d29d5c90f
luanbv <luanbui.dev.bluesky@gmail.com>
2026-08-13 09:48:33 +0700

    fix build apk release

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -80,9 +80,6 @@ android {
     }
 
     packaging {
-        dex {
-            useLegacyPackaging = false
-        }
         jniLibs {
             useLegacyPackaging = false
             // https://issuetracker.google.com/issues/168777344#comment11
